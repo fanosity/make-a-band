320d65693b9597add5935f6f2e5736863dd501c1
Sam Gerendasy <samgerendasy@gmail.com>
2019-03-11 20:17:16 -0700

    Added an empty hamburger menu to all pages.

--- mabrn/src/Router.js
+++ mabrn/src/Router.js
@@ -5,28 +5,33 @@ import ListItems from './components/ListItems';
 import { Header } from './components/common';
 import HamburgerMenu from './components/common/HamburgerMenu';
 import { StyleSheet, TouchableOpacity, Image } from 'react-native'
-import { Scene, Router, Actions, Route, Schema } from 'react-native-router-flux';
+import { Scene, Router, Actions, Route, Schema, Stack, Drawer } from 'react-native-router-flux';
+import MenuIcon from './image/hamburger_menu.png';
+import DrawerContent from './components/DrawerContent';
 
 
 class RouterComponent extends Component {
 
-
+    stateHandler = (prevState, newState, action) => {
+        console.log('onStateChange: ACTION:', action);
+    }
 
     render(){
         return (
-            <Router>
-            {/* Our root (parent) scene */}
-            <Scene key="root" hideNavBar>
-                <Scene key='auth' titleStyle={{ alignSelf: 'center', flex: 1, textAlign: 'center' }} >
-                    <Scene key='login' component={LoginPage} title="Select Event" navigationBarStyle={styles.navBarStyle} initial
-                            onRight={() => {  }} rightButtonImage={require('./image/hamburger_menu.png')} />
-                </Scene>
-                <Scene key="main" titleStyle={{ alignSelf: 'center', flex: 1, textAlign: 'center' }} navigationBarStyle={styles.navBarStyle} title="Make A Band 2019" >
-                    {/* place in renderBackButton={()=>{}} so Actions.main({onBack: () => {...}}) will work. */}
-                    <Scene key="homeView" component={HomeView} initial />
-                    <Scene key="listData" component={ListItems} />
-                </Scene>
-            </Scene>
+            <Router onStateChange={this.stateHandler} >
+            <Stack key="root" titleStyle={{ alignSelf: 'center' }} hideNavBar>
+                    <Drawer key="drawer" hideNavBar contentComponent={DrawerContent} drawerImage={MenuIcon} drawerWidth={250} drawerPosition="right">
+                        <Stack key='auth' titleStyle={{ alignSelf: 'center', flex: 1, textAlign: 'center' }} >
+                            <Scene key='login' component={LoginPage} title="Select Event" navigationBarStyle={styles.navBarStyle} />
+                        </Stack>
+
+                        <Stack key="main" titleStyle={{ alignSelf: 'center', flex: 1, textAlign: 'center' }} navigationBarStyle={styles.navBarStyle} title="Make A Band 2019" >
+                            {/* place in renderBackButton={()=>{}} so Actions.main({onBack: () => {...}}) will work. */}
+                            <Scene key="homeView" component={HomeView} initial />
+                            <Scene key="listData" component={ListItems} />
+                        </Stack>
+                    </Drawer>
+            </Stack>
         </Router>
         );
     }
--- mabrn/src/components/common/HamburgerMenu.js
+++ mabrn/src/components/common/HamburgerMenu.js
@@ -5,19 +5,31 @@ import { View, Text } from 'react-native';
 
 class HamburgerMenu extends Component{
 
-    render(){
-        return (
-            <Drawer ref={c => this.drawer = c} /** Assigning a drawer object into the class. Keep in mind! */
-              type="overlay"
-              tapToClose
-              openDrawerOffset={0.2}
-              panCloseMask={0.2}
-              closedDrawerOffset={-3}
-              styles={drawerStyles}
-              tweenHandler={(ratio) => ({ main: { opacity: (2 - ratio) / 2 } })}
-            />
-        );
-    }
+    render () {
+    const state = this.props.navigationState
+    const children = state.children
+    return (
+      <Drawer
+        ref='navigation'
+        type='static'
+        open={state.open}
+        acceptPan
+        onOpen={() => NavigationActions.refresh({key: state.key, open: true})}
+        onClose={() => NavigationActions.refresh({key: state.key, open: false})}
+        content={<DrawerContent />}
+        styles={Styles}
+        captureGestures
+        tapToClose
+        openDrawerOffset={100}
+        panCloseMask={0.8}
+        panThreshold={150}
+        negotiatePan
+        tweenHandler={Drawer.tweenPresets.parallax}
+      >
+        <DefaultRenderer navigationState={children[0]} onNavigate={this.props.onNavigate} />
+      </Drawer>
+    )
+  }
 }
 
 const styles = {
